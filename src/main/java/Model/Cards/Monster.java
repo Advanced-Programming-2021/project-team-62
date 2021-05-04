050fba0ba7b99a03aff5d54473232d16a780e233
hosseinmasoodi <hosseinmasoodiii@gmail.com>
2021-05-04 19:23:45 +0430

    add player constructor

--- src/main/java/Model/Cards/Monster.java
+++ src/main/java/Model/Cards/Monster.java
@@ -2,4 +2,7 @@ package Model.Cards;
 
 public class Monster extends Card{
 
+    public Monster(String name, String description, String type, String cardNumber, String cardType) {
+        super(name, description, type, cardNumber, cardType);
+    }
 }
--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,4 +1,8 @@
 package Model;
 
 public class Player {
+    private String username;
+    public Player(String username) {
+        this.username = username;
+    }
 }
