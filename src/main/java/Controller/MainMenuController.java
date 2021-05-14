f92d01c83ecde6d967fc35250ef0eec9ac2d3af5
mohammadesteki <mesteki.me@gmail.com>
2021-05-14 18:58:45 +0430

    mainmune

--- src/main/java/Controller/MainMenuController.java
+++ src/main/java/Controller/MainMenuController.java
@@ -1,4 +1,41 @@
 package Controller;
 
+import View.LoginMenuView;
+import View.MainMenuView;
+import View.Regex;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainMenuController {
+    private String command;
+
+    public MainMenuController (String command){
+        this.command=command;
+    }
+
+    public Matcher getMatcher(String command, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(command);
+        return matcher;
+    }
+
+    public String run(){
+        ArrayList<String> menuRegex= Regex.getMenuRegex();
+        if(getMatcher(command, menuRegex.get(0)).find()){
+            return getMatcher(command, menuRegex.get(0)).group(1);
+        }
+        else if(getMatcher(command, menuRegex.get(1)).find()){
+            return "exit";
+        }
+        else if(getMatcher(command, menuRegex.get(2)).find()){
+            new MainMenuView().printing("Main Menu");
+            return "continue";
+        }
+        else{
+            new MainMenuView().printing("invalid command");
+            return "continue";
+        }
+    }
 }
--- src/main/java/View/MainMenuView.java
+++ src/main/java/View/MainMenuView.java
@@ -1,10 +1,52 @@
 package View;
 
+import Controller.MainMenuController;
+import Model.Player;
+
 import java.util.Scanner;
 
 public class MainMenuView {
 
-    public void run(Scanner scanner){
+    public void run(Scanner scanner, Player player){
+        while(true){
+            String command = scanner.nextLine();
+            command=command.trim();
+            String result= new MainMenuController(command).run();
+            if(result.equals("exit")){
+                return;
+            }
+            else if(result.equals("continue")){
+                continue;
+            }
+            else{
+                if(result.equals("duel menu")){
 
+                }
+                else if(result.equals("deck menu")){
+
+                }
+                else if(result.equals("scoreboard menu")) {
+
+                }
+                else if(result.equals("profile menu")){
+
+                }
+                else if(result.equals("shop menu")){
+
+                }
+                else if(result.equals("import/export menu")){
+
+                }
+                else{
+                    printing("invalid command");
+                    continue;
+                }
+            }
+        }
+
+
+    }
+    public void printing(String result){
+        System.out.println(result);
     }
 }
